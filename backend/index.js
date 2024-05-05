e9b66dfec11d554ac832a2fb777833bc5d58cc32
TeDi-A <tediadjene@gmail.com>
2024-05-05 01:04:20 +0100

    parse

--- backend/index.js
+++ backend/index.js
@@ -5,33 +5,35 @@ const app = express()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
-const cors = require('cors')
-app.use(cors())
+app.use(express.static(__dirname + "/dist"))
 
-let corsOptions = {
-    origin: ['http://localhost:4000/api/workouts'],
-}
+// const cors = require('cors')
+// app.use(cors())
+
+// let corsOptions = {
+//     origin: ['http://localhost:4000/api/workouts'],
+// }
 
 //Middleware 
 app.use(express.json())
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.get('/', (req, res) => {
-    res.json("Welcome")
-})
+// app.get('/', (req, res) => {
+//     res.json("Welcome")
+// })
 
 app.use('/api/workouts', workoutRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(PORT, () => {
-            console.log('Listening on port ' + PORT)
-        })
+            app.listen(PORT, () => {
+                console.log('Listening on port ' + PORT)
+            })
     })
     .catch(error => {
         console.log(error)
