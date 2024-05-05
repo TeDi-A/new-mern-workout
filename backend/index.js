fb5af56f888c8ac798d8b5b417e940589cf7a4e5
TeDi-A <tediadjene@gmail.com>
2024-05-05 01:29:17 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,18 @@ require('dotenv').config()
 const express = require('express')
 
 const app = express()
+app.use(express.static(__dirname + "/dist"))
+
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
-app.use(express.static(__dirname + "/dist"))
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+
 app.use('/api/workouts', workoutRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
